1aa47d131898981c0da569d3b579cfb46edbdda4
Akshayku005 <akshaysportive@gmail.com>
2022-04-11 09:18:46 +0530

    Linkedlist

---
+++ src/workshop/Linky.java
@@ -0,0 +1,25 @@
+package workshop;
+
+import java.util.LinkedList;
+
+public class Linky {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+LinkedList<Integer> Linky=new LinkedList<Integer>();
+Linky.add(6);
+Linky.add(8);
+Linky.add(9);
+Linky.add(10);
+Linky.addFirst(20);
+Linky.removeLast();
+Linky.add(0,12);
+for (int i=0; i<Linky.size(); i++) {
+	System.out.print(Linky.get(i));
+	System.out.print(",");
+}
+	}
+
+}
+//WAP to insert elements into the linked list at the first and last position.remove first element from list and  
+//swap two elements in a linked list.
